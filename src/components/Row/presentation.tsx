ac5b12a6ba64f345949543df29f4af4f50d9f968
Pen Peen <ikegami1022.h@gmail.com>
2024-06-29 13:20:59 +0900

    :art: Rowコンポーネントに対するスタイルを適用する

--- src/components/Row/presentation.tsx
+++ src/components/Row/presentation.tsx
@@ -12,12 +12,15 @@ const image_prefix = 'https://image.tmdb.org/t/p/original';
 
 const RowPresentation: FC<Props> = ({ title, movies, isLarge }) => {
   return (
-    <div>
+    <div className="ml-5 text-white">
       <h2>{title}</h2>
-      <div>
+      <div className="flex overflow-y-hidden overflow-x-scroll p-5 scrollbar-hide">
         {movies.map((movie) => (
           <img
             key={movie.id}
+            className={`object-contain w-full max-h-24 m-2 transform transition-transform duration-450 ${
+              isLarge ? 'max-h-60 hover:scale-110' : 'hover:scale-108'
+            }`}
             src={`${image_prefix}${
               isLarge ? movie.poster_path : movie.backdrop_path
             }`}
