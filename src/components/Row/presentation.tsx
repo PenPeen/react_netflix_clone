6c5faf9388f395ae213edb484dab1c9878a68547
Pen Peen <ikegami1022.h@gmail.com>
2024-07-02 21:43:50 +0900

    :sparkles: クリック時にYoutubeトレイラーを表示する

--- src/components/Row/presentation.tsx
+++ src/components/Row/presentation.tsx
@@ -1,16 +1,41 @@
 import { FC } from 'react';
 
+import YouTube from 'react-youtube';
+
 import { Movie } from '@/type/movie';
 
 type Props = {
   title: string;
   movies: Movie[];
   isLarge?: boolean;
+  trailerUrl: string | null;
+  handleClick: (movie: Movie) => Promise<void>;
+};
+
+type Options = {
+  height: string;
+  width: string;
+  playerVars: {
+    autoplay: 0 | 1 | undefined;
+  };
 };
 
 const image_prefix = 'https://image.tmdb.org/t/p/original';
+const opts: Options = {
+  height: '390',
+  width: '640',
+  playerVars: {
+    autoplay: 1,
+  },
+};
 
-const RowPresentation: FC<Props> = ({ title, movies, isLarge }) => {
+const RowPresentation: FC<Props> = ({
+  title,
+  movies,
+  isLarge,
+  trailerUrl,
+  handleClick,
+}) => {
   return (
     <div className="ml-5 text-white">
       <h2>{title}</h2>
@@ -25,9 +50,11 @@ const RowPresentation: FC<Props> = ({ title, movies, isLarge }) => {
               isLarge ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
